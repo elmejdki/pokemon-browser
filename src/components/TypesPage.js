2db4aeb3d169fc9ad64faaf73e84f9c6bed1c80c
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2021-02-08 00:13:26 +0100

    Finish working on the Types Page Component

--- src/components/TypesPage.js
+++ src/components/TypesPage.js
@@ -1,14 +1,67 @@
+import { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import Loader from './Loader';
 import {
   wrapper,
   marginTop,
 } from '../stylesheets/Common.module.css';
+import {
+  container,
+} from '../stylesheets/HomePage.module.css';
 
-const TypesPage = () => (
-  <div
-    className={`${wrapper} ${marginTop}`}
-  >
-    TypesPage
-  </div>
-);
+const TypesPage = () => {
+  const { name } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${name}`);
+
+    const data = await response.json();
+
+    const { pokemon } = data;
+
+    const result = [];
+    let index = 0;
+
+    while (index < pokemon.length) {
+      if (index < 20) {
+        result.push(pokemon[index]);
+      } else {
+        break;
+      }
+
+      index += 1;
+    }
+
+    setPokemons(result.map(obj => obj.pokemon.name));
+    setLoading(false);
+  }, []);
+
+  return (
+    <div
+      className={`${wrapper} ${marginTop}`}
+    >
+      {
+        loading ? (
+          <Loader />
+        ) : (
+          <div className={container}>
+            {
+              pokemons.map(pokemon => (
+                <Link
+                  key={pokemon}
+                  to={`/pokemon/${pokemon}`}
+                >
+                  {pokemon}
+                </Link>
+              ))
+            }
+          </div>
+        )
+      }
+    </div>
+  );
+};
 
 export default TypesPage;
